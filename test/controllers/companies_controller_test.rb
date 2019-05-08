29a908eff370cb46b558516d14f613f342e59c8b
Hannah Carnes <hecarnes@gmail.com>
2019-05-08 17:06:05 -0400

    companies index renders all companies as json

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class CompaniesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get companies_index_url
-    assert_response :success
+  setup do
+    @companies = Company.all
   end
 
+  test "should list companies as json" do
+   get companies_url, as: :json
+   assert_response :success
+   
+   @companies.each do |company|
+     assert_includes(response.parsed_body, {id: company.id, name: company.name, latitude: company.latitude, longitude: company.longitude}.stringify_keys)
+   end
+ end
 end
