8374507c0f8f2a256b3dfb0c1cb506c83708db99
Hannah Carnes <hecarnes@gmail.com>
2019-05-08 17:12:09 -0400

    import companies.json seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+json = ActiveSupport::JSON.decode(File.read('db/companies.json'))
+
+json.each do |company|
+  Company.create!(name: company['company'], latitude: company['latitude'], longitude: company['longitude'])
+end
