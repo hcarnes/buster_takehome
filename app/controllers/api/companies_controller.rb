5d56e4878bf23b8852a93ef51cb2e7ea149379e8
Hannah Carnes <hecarnes@gmail.com>
2019-05-09 11:36:45 -0400

    scaffold crud actions in company controller and update routes

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -2,4 +2,33 @@ class Api::CompaniesController < ApplicationController
   def index
     render json: Company.all
   end
+
+  def show
+    render json: Company.find(params[:id])
+  end
+
+  def create
+    render json: Company.create(company_params)
+  end
+
+  def destroy
+    render json: Company.destroy(params[:id])
+  end
+
+  def update
+    company = Company.find(params['id'])
+    company.update(company_params)
+    respond_with Company, json: company
+  end
+
+  private
+  
+  def company_params
+    params.require(:company).permit(
+      :id,
+      :name,
+      :latitude,
+      :longitude
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'site#index'
-  
+  root to: redirect('/companies')
+
+  get 'companies', to: 'site#index'
+  get 'companies/new', to: 'site#index'
+  get 'companies/:id', to: 'site#index'
+  get 'companies/:id/edit', to: 'site#index'
+
+
   namespace :api do
     get '/companies', to: 'companies#index'
   end
