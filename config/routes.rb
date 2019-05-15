6001dd5c2462eab3335c09a2290654f25fe0e868
Hannah Carnes <hecarnes@gmail.com>
2019-05-15 11:27:16 -0400

    route all web requests to site#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect('/companies')
 
-  get 'companies', to: 'site#index'
-  get 'companies/new', to: 'site#index'
-  get 'companies/:id', to: 'site#index'
-  get 'companies/:id/edit', to: 'site#index'
-
-
   namespace :api do
     resources :companies
   end
+
+  get '*path', to: 'site#index'
 end
